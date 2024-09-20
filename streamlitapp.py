7b78e3f867306d11376ceabe161fe01fd6bddf1d
iiakshat <jakshat569@gmail.com>
2024-09-20 22:04:15 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -11,34 +11,50 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-
 from dotenv import load_dotenv
 load_dotenv()
 
 groq_api_key = os.getenv("GROQ_API_KEY")
 os.environ['Google_API_KEY'] = os.getenv("GOOGLE_API_KEY")
 
-st.title('Cobra.Ai')
-
 llm = ChatGroq(groq_api_key=groq_api_key, model='llama3-8b-8192')
 
-def vector_embedding():
+def vector_embedding(flask=False):
 
-    try:
-        if imquery:
-            return 
+    print("Embedding Started.")
+    if flask:
+        try: 
+            
+            embeddings = GoogleGenerativeAIEmbeddings(model='models/embedding-001')
+            loader = PyPDFDirectoryLoader("./files")
+            docs = loader.load()
+            print("Files loaded.")
+
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+            final_document = text_splitter.split_documents(docs)
+            vectors = FAISS.from_documents(final_document, embeddings)
+            print("Embedding Done.")
+            return vectors
         
-        if "vectors" not in st.session_state:
-            st.session_state.embeddings = GoogleGenerativeAIEmbeddings(model='models/embedding-001')
-            st.session_state.loader = PyPDFDirectoryLoader("./files")
-            st.session_state.docs = st.session_state.loader.load()
-            st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-            st.session_state.final_document = st.session_state.text_splitter.split_documents(st.session_state.docs)
-            st.session_state.vectors = FAISS.from_documents(st.session_state.final_document, st.session_state.embeddings)
-            st.success("Memorised Everything.", icon="✅")
-    
-    except:
-        pass
+        except:
+            print("Embedding Failed.")
+
+    else:
+        try:
+            if imquery:
+                return 
+            
+            if "vectors" not in st.session_state:
+                st.session_state.embeddings = GoogleGenerativeAIEmbeddings(model='models/embedding-001')
+                st.session_state.loader = PyPDFDirectoryLoader("./files")
+                st.session_state.docs = st.session_state.loader.load()
+                st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+                st.session_state.final_document = st.session_state.text_splitter.split_documents(st.session_state.docs)
+                st.session_state.vectors = FAISS.from_documents(st.session_state.final_document, st.session_state.embeddings)
+                st.success("Memorised Everything.", icon="✅")
+        
+        except:
+            pass
 
 def clndir(d = './files/'):
     
@@ -57,16 +73,13 @@ def download_file(query, file_path="output.txt"):
     with open("output.pdf", "rb") as f:
         data = f.read()
 
+    os.remove("output.pdf")
     st.download_button(
         label = "Download",
         data = data,
-        file_name = query + " - CobraAi.pdf",
+        file_name = query + " | CobraAi.pdf",
         mime = "text/plain",
     )
-
-docs = st.file_uploader(label="Upload Document", type=["jpg", "jpeg", "png", "pdf"], accept_multiple_files=True)
-
-preview = st.checkbox("Preview")
 
 prompt = ChatPromptTemplate.from_template(
     '''
@@ -95,85 +108,91 @@ prompt2 = ChatPromptTemplate.from_template(
     <context> {context} <context>
     """)
 
-if preview:
-    ft.display(docs)
+if __name__ == "__main__":
 
-if docs:
-    if docs[0].name.endswith(".pdf"):
-        for doc in docs:
-            if doc.name.endswith(".pdf"):
-                with open(os.path.join("files",doc.name),"wb") as f:
-                    f.write(doc.getbuffer())
+    st.title('Cobra.Ai')
+    docs = st.file_uploader(label="Upload Document", type=["jpg", "jpeg", "png", "pdf"], accept_multiple_files=True)
+    preview = st.checkbox("Preview")
 
-        st.success(f"Success: Saved {len(docs)} file(s).", icon="✅")
+    if preview:
+        ft.display(docs)
 
-        imquery = st.checkbox("My query is related to only images in the pdf.")
-        query = st.text_input("Question", placeholder="Ask Me Anything From PDF.")
+    if docs:
+        if docs[0].name.endswith(".pdf"):
+            for doc in docs:
+                if doc.name.endswith(".pdf"):
+                    with open(os.path.join("files",doc.name),"wb") as f:
+                        f.write(doc.getbuffer())
+
+            st.success(f"Success: Saved {len(docs)} file(s).", icon="✅")
+
+            imquery = st.checkbox("My query is related to only images in the pdf.")
+            query = st.text_input("Question", placeholder="Ask Me Anything From PDF.")
 
 
-        if imquery:
+            if imquery:
+                try:
+                    if preview:
+                        ft.display(docs, imquery)
+
+                    images = ft.details(docs)
+                    com_response = "\n".join(ft.get_response(images=images, query=query))
+
+                    chain = prompt2 | llm
+                    s = time.perf_counter()
+                    response = chain.invoke({'input': query, 'context': com_response})
+
+                    if query:
+                        res = dict(response)["content"]
+                        st.write(res)
+                        io.write_to_file(query, res)
+                        f = time.perf_counter()
+                        st.divider()
+                        st.caption(f"Time taken : {round(f-s,2)} seconds")
+                        download_file(query)
+                    
+                    clndir()
+
+                except Exception as e:
+                    print("Error : ",e)
+
+            else:
+                vector_embedding()
+                clndir()
+
+                if query and not imquery:
+                    s = time.perf_counter()
+                    document_Chain = create_stuff_documents_chain(llm, prompt)
+                    retriever = st.session_state.vectors.as_retriever()
+                    retireval_Chain = create_retrieval_chain(retriever, document_Chain)
+
+                    response = retireval_Chain.invoke({'input':query})
+                    res = response['answer']
+                    st.write(res)
+                    f = time.perf_counter()
+                    st.divider()
+                    st.caption(f"Time taken : {round(f-s,2)} seconds")
+                    io.write_to_file(query, res)
+                    download_file(query)
+
+        else:
             try:
-                if preview:
-                    ft.display(docs, imquery)
-
                 images = ft.details(docs)
+                query = st.text_input("Question", placeholder="Ask Me About Image(s)")
                 com_response = "\n".join(ft.get_response(images=images, query=query))
 
                 chain = prompt2 | llm
                 s = time.perf_counter()
                 response = chain.invoke({'input': query, 'context': com_response})
-
                 if query:
                     res = dict(response)["content"]
                     st.write(res)
-                    io.write_to_file(query, res)
                     f = time.perf_counter()
                     st.divider()
                     st.caption(f"Time taken : {round(f-s,2)} seconds")
+                    io.write_to_file(query, res)
                     download_file(query)
-                
-                clndir()
 
             except Exception as e:
-                print("Error : ",e)
-
-        else:
-            vector_embedding()
-            clndir()
-
-            if query and not imquery:
-                s = time.perf_counter()
-                document_Chain = create_stuff_documents_chain(llm, prompt)
-                retriever = st.session_state.vectors.as_retriever()
-                retireval_Chain = create_retrieval_chain(retriever, document_Chain)
-
-                response = retireval_Chain.invoke({'input':query})
-                res = response['answer']
-                st.write(res)
-                f = time.perf_counter()
-                st.divider()
-                st.caption(f"Time taken : {round(f-s,2)} seconds")
-                io.write_to_file(query, res)
-                download_file(query)
-
-    else:
-        try:
-            images = ft.details(docs)
-            query = st.text_input("Question", placeholder="Ask Me About Image(s)")
-            com_response = "\n".join(ft.get_response(images=images, query=query))
-
-            chain = prompt2 | llm
-            s = time.perf_counter()
-            response = chain.invoke({'input': query, 'context': com_response})
-            if query:
-                res = dict(response)["content"]
-                st.write(res)
-                f = time.perf_counter()
-                st.divider()
-                st.caption(f"Time taken : {round(f-s,2)} seconds")
-                io.write_to_file(query, res)
-                download_file(query)
-
-        except Exception as e:
-            st.error(f"Error: {e} \n\n Try Changing the input.", icon="🚨")
-            print(e)
+                st.error(f"Error: {e} \n\n Try Changing the input.", icon="🚨")
+                print(e)
